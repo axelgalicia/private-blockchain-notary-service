5ae6fffc5c693b360e6a14f4a80edad5e0f7419b
Axel Galicia <axelgalicia@gmail.com>
2018-12-10 07:21:30 -0500

    Adding to the timeoutRequest array

--- db/memPool.js
+++ db/memPool.js
@@ -1,22 +1,47 @@
 /*======= MemPool class =============
 |  Class that represents the mempool |
 ====================================*/
+
+const RequestObject = require('../entities/RequestObject');
+
 class MemPool {
     constructor() {
-        this.mempool = [];
+        this.memPoolValid = [];
+        this.memPool = [];
         this.timeoutRequests = [];
     }
 
-    addRequestToPool(request) {
-        this.timeoutRequests[request.walletAddress] = setTimeout(() => { this.removeValidationRequest(request.walletAddress) }, MemPool.TimeoutRequestsWindowTime);
+    setTimeout(request) {
+        this.timeoutRequests[request.walletAddress] =
+            setTimeout(() => { this.removeValidationRequest(request.walletAddress) }, MemPool.TimeoutRequestsWindowTime);
     }
 
-    removeValidationRequest(address) {
-        console.log(`Remove Address: ${address}`);
+    // Add the request validation
+    addRequestValidation(request) {
+        let request = this.getExistingRequest(request);
+        if (request) {
+            console.log(exist);
+        } else {
+            this.setTimeout(request);
+        }
     }
+
+    // Remove the request from the mempool
+    removeValidationRequest(walletAddress) {
+        // this.timeoutRequests = this.timeoutRequests.filter(r => r.walletAddress !== walletAddress);
+        console.log(`Remove request with address: ${walletAddress}`);
+
+    }
+
+    getExistingRequest(request) {
+        if (this.timeoutRequests.includes(request.walletAddress)) {
+            return this.timeoutRequests[request.walletAddress];
+        }
+        return null;
+    }
+
 
 }
-
 
 MemPool.TimeoutRequestsWindowTime = 3000;
 module.exports = MemPool;
